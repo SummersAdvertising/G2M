5ae628c81d45244a5f70b50f27f306a3f054b6c1
HanaHsu <kobanae@gmail.com>
2014-02-17 12:46:37 +0800

    加上for_iframe頁

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,8 @@
+#encoding: utf-8
 class Record < ActiveRecord::Base
   attr_accessible :answer, :fbshare_id, :user_fbid, :user_type
+  validates_length_of :answer, :minimum => 18, :message => "不能空白"
+  validates :user_type, :presence => true
 
   has_one :signup
 end
