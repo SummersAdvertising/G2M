7c3c8f83f3eb39b040c0f6802d59da39ddddf22d
HanaHsu <kobanae@gmail.com>
2014-02-18 14:04:18 +0800

    修正完成後redirect位址

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -189,10 +189,10 @@ class RecordsController < ApplicationController
         @signup.update_attributes(params[:signup])
 
         if params[:from] == "iframe"
-          format.html { redirect_to new_for_iframe_records_path, notice: '參加成功。' }
+          format.html { redirect_to "http://www.microsoft.com/taiwan/promo/eq/Get2Modern/" }
           format.json { head :no_content }
         else
-          format.html { redirect_to root_path, notice: '參加成功。' }
+          format.html { redirect_to "http://www.microsoft.com/taiwan/promo/eq/Get2Modern/" }
           format.json { head :no_content }
         end
 
